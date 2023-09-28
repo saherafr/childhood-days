7716ccc677285307b772f145ecdc91a546286e67
saherafr <saherafr@ualberta.ca>
2023-09-28 07:59:23 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Doraemon',
+    job: 'Spread love',
+    image: 'https://m.media-amazon.com/images/I/51yWHkxB4ML._AC_SY550_.jpg',
+    text: "Doraemon, you are a timeless frienddWith your pocket full of gadgets, you transcend.Through laughter and tears, you've always been there,In the pages of our childhood, a constant, so rare",
+    
+    
+  },
+  {
+    id: 2,
+    name: 'Tom and Jerry',
+    job: 'Source of laughter',
+    image: 'https://m.media-amazon.com/images/I/61cdXlJSqWL._SX450_.jpg',
+    text: 'Your character reminds us that its okay to try and fail, as long as we keep getting back up and trying again.Jerry, your clever wit and resourcefulness have shown us that intelligence and quick thinking can overcome even the toughest of challenges',
+  },
+  {
+    id: 3,
+    name: 'Pika Pika',
+    job: 'A friend',
+    image: 'https://m.media-amazon.com/images/I/51X1I7W4cPL._AC_SX569_.jpg',
+    text: 'You are more than just a Pokémon; you are a symbol of friendship, adventure, and unwavering determination. With your adorable yellow visage, rosy cheeks, and that unmistakable lightning bolt-shaped tail, you have  charmed your way into the hearts of fans young and old.',
+  },
+  {
+    id: 4,
+    name: 'PowerPuff Girls',
+    job: 'Teamwork makes the dream work',
+    image: 'https://cdn.britannica.com/08/190808-050-CB26C47B/The-Powerpuff-Girls-Bubbles-Blossom-Buttercup.jpg',
+    text: 'The Powerpuff Girls, you have shown us that girls can be heroes, leaders, and role models. You have shattered stereotypes and proved that strength knows no gender. Thank you for being a symbol of girl power and reminding us that anyone can be a hero, regardless of age or size.',
+  },
+];
+
+export default reviews;
